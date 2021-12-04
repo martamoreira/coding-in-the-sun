b176aef66eed787b4473feec705e9944b121101c
Marta Moreira <marta.p-moreira@live.com.pt>
2021-12-04 11:52:49 +0000

    CSS: styled min temp (opacity)

--- src/index.js
+++ src/index.js
@@ -51,10 +51,10 @@ function displayForecast(response) {
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          )}° </span>
+          )}°</span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
-          )}° </span>
+          )}°</span>
         </div>
       </div>
  `;
